e935219710818b75b24e338cd08d8978e5152d24
TakayukiKomada <Takakoma.79@gmail.com>
2022-07-02 17:10:54 +0900

    わけた動作確認OK

--- 01.py
+++ 01.py
@@ -3,14 +3,14 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def index():
-    if request.method == "GET":
-        return render_template("index.html")
-    if request.method == "POST":
-        message = request.form["message"]
-        
-        return render_template("index.html", message=message)
+    return render_template("index.html")
+
+@app.route("/", methods=["POST"])
+def index_post():
+    message = request.form["message"]
+    return render_template("index.html", message=message)
 
 
 if __name__ == "__main__":
